bf5e32b3814155e58c7bb0b9ba441595ee2a5374
yaelajami2 <y0556722091@gmail.com>
2024-09-19 09:50:54 +0300

    first commit

---
+++ UnitTestProject1/UnitTest_one.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+  
+    public class UnitTest1
+    { 
+        [Fact]
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var x = new apartmentController();
+           Assert.IsInstanceOfType<List<apartment>>();
+        }
+    }
+}
