05fbab9a9efdb2cea2712bfeef946c93ab64ce5b
yaelajami2 <y0556722091@gmail.com>
2024-12-17 14:36:29 +0200

    send_mail

---
+++ Controllers/User_LogController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Net.Mail;
+using System.Net;
+using WebApplication14.Models;
+
+namespace WebApplication14.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class User_LogController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly MongoDBManager<Log_user> _managQuery;
+        public User_LogController(IConfiguration configuration)
+        {
+            _managQuery = new MongoDBManager<Log_user>(configuration, "Log_user");
+        }
+
+        [HttpGet]
+        public ActionResult<List<Log_user>> Get()
+        {
+            var collection = _managQuery.GetCollectionByName<Log_user>();
+            return collection.Find(_ => true).ToList();
+        }
+
+        [HttpPost("insertById")]
+        public async Task<IActionResult> Insert_row([FromBody] Log_user log_User)
+        {
+            try
+            {
+                await SendEmailAsync(log_User); // קריאה לשיטה אסינכרונית
+                log_User.Id = null; // MongoDB will generate the Id automatically
+                await _managQuery.InsertAsync(log_User);
+                return CreatedAtAction(nameof(GetById), new { id = log_User.Id }, log_User);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        private async Task SendEmailAsync(Log_user log_User)
+        {
+            try
+            {
+        
+                MailMessage message = new MailMessage("apartment0556722091@gmail.com", "y0556722091@gmail.com");
+
+
+                message.Subject = "צפו באתר שלך";
+                message.Body = $"Date: {log_User.Date}, ScreenInfo: {log_User.ScreenInfo}, location: {log_User.location}";
+                
+
+                using (SmtpClient client = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    client.Credentials = new NetworkCredential("tripyaeleden@gmail.com", "anpckkcheirpwdbv");
+                    client.EnableSsl = true;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                    await client.SendMailAsync(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error sending email: {0}", ex.Message);
+            }
+        }
+
+
+        [HttpGet("getById/{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            try
+            {
+                var result = await _managQuery.QueryByIdAsync(id);
+                if (result == null)
+                {
+                    return NotFound("Document not found");
+                }
+
+                // Return serializable object (e.g., a DTO or BsonDocument)
+                return Ok(result.ToJson());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ Models/Log_user.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace WebApplication14.Models
+{
+    public class Log_user
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("location")]
+        public string location { get; set; }
+        [BsonElement("Date")]
+        public string Date { get; set; }
+        [BsonElement("ScreenInfo")]
+        public string ScreenInfo { get; set; }
+
+    }
+}
