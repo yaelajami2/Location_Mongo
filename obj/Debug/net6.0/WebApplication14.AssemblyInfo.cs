4227d26df71cc0b08a6a9581f4ab62aef0ccabfd
yaelajami2 <y0556722091@gmail.com>
2025-01-12 00:13:42 +0200

    mail

--- obj/Debug/net6.0/WebApplication14.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebApplication14.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b19b4773bbb1e0d45436d3e407190126681424")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+066de2f92cbf7b0ce2e459bbd4f9439665d2e30a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
